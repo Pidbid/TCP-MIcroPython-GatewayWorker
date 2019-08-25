a9d031ff4f3f3d58320435cd74abc024f93573b2
Pidbid <35859787+Pidbid@users.noreply.github.com>
2019-08-25 18:46:01 +0800

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,16 @@
+from machine import Pin
+
+import socket
+a = Pin(15,Pin.OUT)
+s=socket.socket()
+s.connect(('TESTIP',8282))
+print('connection is successful')
+s.send('wicos')
+while True:
+	data = s.recv(500).decode('utf8')
+	if data == '1':
+		a.value(1)
+		print('The led is on')
+	if data == '0':
+		a.value(0)
+		print('The led is off')
